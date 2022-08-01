483468cdf924a6cebbebab47bc48ace60c6e9942
Shane Allison <shane@publicaddress.ai>
2022-08-01 17:59:03 +1000

    Fixed names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Public Address'
+copyright = '2022, Public Address'
+author = 'Public Address'
 
 release = '0.1'
 version = '0.1.0'
